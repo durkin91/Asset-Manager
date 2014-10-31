4bdf7d7e52f2003a4674a64bf0e9f15a61400730
Nikki Durkin <nikki@99dresses.com>
2014-10-31 22:48:37 +1100

    Added in copy to clipboard

---
+++ Asset Manager/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Asset Manager
+//
+//  Created by Nikki Durkin on 10/31/14.
+//  Copyright (c) 2014 Nikki Durkin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainViewController : NSViewController
+
+@end
---
+++ Asset Manager/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  Asset Manager
+//
+//  Created by Nikki Durkin on 10/31/14.
+//  Copyright (c) 2014 Nikki Durkin. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
